1af6cfdc0678a50ec7867bfc36c1ac7f2154b285
Ian Kar <ian@example.com>
2025-08-29 23:52:37 -0700

    fix(map): correct react-map-gl v8 import syntax for mapbox subpath

--- components/panels/LARestaurantsMap.tsx
+++ components/panels/LARestaurantsMap.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import Map, { Marker, Popup } from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 type LARestaurant = {
